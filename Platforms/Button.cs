81943aa3cbcefc1e2a3f4afab86e9c3586431b06
Alex Joseph <alex.joseph5575@gmail.com>
2019-04-17 21:41:07 -0700

    Button Class Outline

---
+++ Platforms/Button.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Platforms
+{
+    class Button
+    {
+        Game1 Game1;
+        GraphicsDevice GraphicsDevice;
+        Texture2D Texture;
+        Vector2 Position;
+        Color Color;
+        int height;
+        int width;
+        string text;
+        MouseState prevMouseState;
+        Rectangle button;
+
+        public bool clicked = false;
+
+        public Button(Game1 game, string text, int height, int width, Vector2 position)
+        {
+            Game1 = game;
+            GraphicsDevice = game.GraphicsDevice;
+            Color = Color.Black;
+            this.text = text;
+            this.height = height;
+            this.width = width;
+            Texture = new Texture2D(GraphicsDevice, width, height);
+            Position = position;
+            prevMouseState = Mouse.GetState();
+            button = new Rectangle((int)Position.X, (int)Position.Y, this.width, this.height);
+        }
+
+        public void StartButton()
+        {
+
+        }
+
+        public void ExitButton()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (!clicked)
+            {
+                MouseState curMouseState = Mouse.GetState();
+                Point mousePosition = curMouseState.Position;
+                if (button.Contains(mousePosition))
+                {
+                    if (curMouseState.LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released)
+                    {
+                        if (text.Equals("Start"))
+                        {
+                            StartButton();
+                        }
+                        else
+                        {
+                            ExitButton();
+                        }
+                        clicked = true;
+                    }
+                }
+                prevMouseState = curMouseState;
+            }
+        }
+    }
+}
